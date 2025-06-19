4af7a3f5abd3e9a828552a0cad9e11252587458e
hobbyisu789 <isitaminu@gmail.com>
2025-06-19 08:52:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 26318274
 API_HASH     = "585f48485cf7419844c398d5da226545"
-BOT_TOKEN    = "7411939836:AAE1NOS-aIvyvSH7ga0GFVxND73hYNDJ97s"
+BOT_TOKEN    = ""
 SESSION      = "BQGxBBoACndCUKWX1IlYrFNbTYe6j5PTtqWBL2dOvrw8Yn8NupfujdeXCuHga-tpMfSxKV6fzVdkTzFnX1K_hdI2sdf1subvWOoU8fhg9FefRNeYU36LwgGX1YumHPwKdSSSzvju3K3-Tt-zjNyaopif-EEbGaNScdGTernyF_eC0oz-iBiKwKWYrhtTsiGbEyiVBOMb6AXVk_S-NWIGrKwiDNyqtGLJcwBeqCrUmTZf5kbYmRhv-Ft8Mc9kaD_fRb4WyS1UWtXyg-zgiagYZ_DDWQEYUvELg05DSVV4ndoCb-qM0yzlqCi1NvPAYx0CMko8uVzcnr7xsLOYQkOdYd5NlXDl7wAAAAGOoXC7AA"
 DATABASE_URI = "mongodb+srv://mangolv:uBM0qfRO8YqzlqKH@cluster0.vk1kdu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1002017490016
